f16b737a1eeb1c522da11c415d1c526b25aee3f4
Revan <154826103+AZachia@users.noreply.github.com>
2024-02-27 14:25:31 +0100

    update demo

--- new_demo.py
+++ new_demo.py
@@ -1,12 +1,12 @@
-import new_colors as colors
-from new_colors import cprint
+import color
+from color import cprint
 
 print("\nNormal print:")
-print(colors.red, colors.underline, "Hello ", colors.green, "World !")
-print(f"{colors.reset}↑↑      ↑↑\nWierd spaces (expected only the first space) and you have to manually reset the color at the end")
+print(color.red, color.underline, "Hello ", color.green, "World !")
+print(f"{color.reset}↑↑      ↑↑\nWierd spaces (expected only the first space) and you have to manually reset the color at the end")
 
 print("\ncprint:")
-clrless = cprint(colors.red, colors.underline, "Hello ", colors.green, "World !")
+clrless = cprint(color.red, color.underline, "Hello ", color.green, "World !")
 print("No wierd spaces, and no need to manually reset color")
 print("\nAnd you can also extract a colorless version of the printed text:")
 print(clrless)
